21d7d9dfe1625c97419d62fe63ed4357e6e79e92
Saqib <saqibomer.cs@gmail.com>
2022-05-17 17:54:00 +0500

    - Added utility classes for Loadable, CancelBag and NetwrokHelper - Added Networking logic and WebRepository

---
+++ scalio-test-combine/Utilities/CancelBag.swift
@@ -0,0 +1,24 @@
+//
+//  CancelBag.swift
+//  scalio-test-combine
+//
+//  Created by TOxIC on 17/05/2022.
+//
+
+import Combine
+
+final class CancelBag {
+    fileprivate(set) var subscriptions = Set<AnyCancellable>()
+    
+    func cancel() {
+        subscriptions.removeAll()
+    }
+}
+
+extension AnyCancellable {
+    
+    func store(in cancelBag: CancelBag) {
+        cancelBag.subscriptions.insert(self)
+    }
+}
+
