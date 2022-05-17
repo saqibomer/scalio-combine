21e5e12dbd195768eb6b74a5c9fbfa07a22539ae
Saqib <saqibomer.cs@gmail.com>
2022-05-17 13:41:39 +0500

    - Added models and mocked data

---
+++ scalio-test-combine/Models/User.swift
@@ -0,0 +1,44 @@
+//
+//  User.swift
+//  scalio-test-combine
+//
+//  Created by TOxIC on 17/05/2022.
+//
+
+import Foundation
+
+struct User: Codable {
+    
+    var login: String
+    var id: Int
+    var avatarUrl: String
+    var profileUrl: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case login
+        case id
+        case avatarUrl = "avatar_url"
+        case profileUrl = "url"
+    }
+}
+
+extension User {
+    static let mockedData: [User] = [
+                                        User(
+                                            login: "Saqib",
+                                            id: 1,
+                                            avatarUrl: "https://avatars.githubusercontent.com/u/33384?v=4",
+                                            profileUrl: "https://api.github.com/users/foo"),
+                                        
+                                        User(
+                                            login: "Omer",
+                                            id: 1,
+                                            avatarUrl: "https://avatars.githubusercontent.com/u/83657?v=4",
+                                            profileUrl: "https://api.github.com/users/foosel"),
+                                        User(
+                                            login: "foone",
+                                            id: 1,
+                                            avatarUrl: "https://avatars.githubusercontent.com/u/83657?v=4",
+                                            profileUrl: "https://api.github.com/users/foosel"),
+                                    ]
+}
---
+++ scalio-test-combine/Models/UserResponse.swift
@@ -0,0 +1,31 @@
+//
+//  UserResponse.swift
+//  scalio-test-combine
+//
+//  Created by TOxIC on 17/05/2022.
+//
+
+import Foundation
+
+struct UserResponse: Codable {
+    
+    var totalCount: Int
+    var inCompleteResult: Bool
+    var users: [User]
+    
+    
+    private enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case inCompleteResult = "incomplete_results"
+        case users = "items"
+    }
+}
+
+
+extension UserResponse {
+    
+    static let mockedData: UserResponse = UserResponse(
+                                                        totalCount: User.mockedData.count,
+                                                        inCompleteResult: false,
+                                                        users: User.mockedData)
+}
